e6a2206336f67855ea721b332be1b319c57e70d6
sydt <sydtlph21197@fpt.edu.vn>
2024-01-27 13:41:28 +0700

    [ADD] create project bee_store

---
+++ src/main/java/com/example/bee_store/BeeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.bee_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeeStoreApplication.class, args);
+    }
+
+}
